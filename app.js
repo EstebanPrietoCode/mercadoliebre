afb211e6b5e3cd12699f493ee4539e2fff2fd19b
EstebanPrietoCode <contactoestebanprieto@gmail.com>
2021-09-21 12:07:06 -0300

    Este es el primer commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, './public')));
+
+app.listen(process.env.PORT || 3000, () => 
+    console.log('Servidor funcionando'));
+
+app.get('/', (req,res) => {
+    res.sendFile(path.resolve(__dirname,'./views/home.html'));
+})
+
+app.get('/registro', (req,res) => {
+    res.sendFile(path.resolve(__dirname,'./views/register.html'));
+})
+
+app.get('/login', (req,res) => {
+    res.sendFile(path.resolve(__dirname,'./views/login.html'));
+})
